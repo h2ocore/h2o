d603def5f93c5b6514883800995b5c6d189c45fa
ZioFabry <ziofabry@hotmail.com>
2018-07-20 22:40:52 +0200

    fixed version peer check

--- src/main.cpp
+++ src/main.cpp
@@ -5299,7 +5299,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
         CAddress addrFrom;
         uint64_t nNonce = 1;
         vRecv >> pfrom->nVersion >> pfrom->nServices >> nTime >> addrMe;
-        if (pfrom->nVersion < ActiveProtocol())
+        if (pfrom->nVersion < MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT)
         {
             // disconnect from peers older than this proto version
             LogPrintf("peer=%d using obsolete version %i; disconnecting\n", pfrom->id, pfrom->nVersion);
