7762622d8f09bff7250cbd26f8ebf9bcb2cd127f
ZioFabry <ziofabry@hotmail.com>
2018-07-20 20:53:36 +0200

    makekeypair rpc function implemented

--- src/rpcmisc.cpp
+++ src/rpcmisc.cpp
@@ -981,3 +981,37 @@ UniValue getspentinfo(const UniValue& params, bool fHelp)
 
     return obj;
 }
+
+UniValue makekeypair(const UniValue& params, bool fHelp)
+{
+    if (fHelp || params.size() > 1)
+        throw runtime_error(
+            "makekeypair [prefix]\n"
+            "Make a public/private key pair.\n"
+            "[prefix] is optional preferred prefix for the public key.\n");
+    
+    string strPrefix = "";
+    if (params.size() > 0)
+        strPrefix = params[0].get_str();
+    
+    CKey key;
+    CPubKey pubkey;
+    string pubkeyhex;
+    int nCount = 0;
+    do
+    {
+        key.MakeNewKey(false);
+        nCount++;
+        pubkey = key.GetPubKey();
+        pubkeyhex = HexStr(pubkey.begin(), pubkey.end());
+    } while (nCount < 10000 && strPrefix != pubkeyhex.substr(0, strPrefix.size()));
+    
+    if (strPrefix != pubkeyhex.substr(0, strPrefix.size()))
+        return NullUniValue;
+    
+    UniValue obj(UniValue::VOBJ);
+    obj.push_back(Pair("PublicKey", pubkeyhex));
+    obj.push_back(Pair("PrivateKey", CBitcoinSecret(key).ToString()));
+
+    return obj;
+}
